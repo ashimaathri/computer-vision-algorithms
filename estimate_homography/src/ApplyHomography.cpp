2a0024c77bf7a0ca0dd33bc3d6c9e3531fdafd8b
Ashima <ashimaathri@gmail.com>
2021-01-02 14:50:58 -0500

    homegrown bilinear interpolation. yay!

--- estimate_homography/src/ApplyHomography.cpp
+++ estimate_homography/src/ApplyHomography.cpp
@@ -1,4 +1,5 @@
 #include <mvg.h>
+#include <math.h>
 #include <opencv2/imgproc.hpp>
 using namespace cv;
 using namespace std;
@@ -16,35 +17,76 @@ Mat construct_roi_mask(int height, int width, vector<Point2f> points) {
   return mask;
 }
 
-void copyPerspectively(Mat &to, Mat from, Mat homography, std::vector <Point2f> points) {
+Point2f transform_point(Mat homography, float x, float y) {
+  Point2f transformed_point;
+
+  Mat homogeneous_src_point = (Mat_<float>(3, 1) << x, y, 1);
+  Mat homogeneous_dst_point = homography * homogeneous_src_point;
+
+  float w = homogeneous_dst_point.at<float>(2, 0);
+  transformed_point.x = homogeneous_dst_point.at<float>(0, 0) / w;
+  transformed_point.y = homogeneous_dst_point.at<float>(1, 0) / w;
+
+  return transformed_point;
+}
+
+// Very slow because it goes pixel by pixel instead of using GPU matrix
+// operations
+Vec3b interpolate(Mat image, Point2f point) {
+  float int_x, int_y;
+  float frac_x = modf(point.x, &int_x);
+  float frac_y = modf(point.y, &int_y);
+
+  Vec3b f11 = image.at<Vec3b>(int_y, int_x);
+  Vec3b f12 = image.at<Vec3b>(int_y + 1, int_x);
+  Vec3b f21 = image.at<Vec3b>(int_y, int_x + 1);
+  Vec3b f22 = image.at<Vec3b>(int_y + 1, int_x + 1);
+  Vec3b interpolated_value;
+
+  Mat x_weights = (Mat_<float>(1, 2) << (1 - frac_x), frac_x);
+  Mat y_weights = (Mat_<float>(2, 1) << (1 - frac_y), frac_y);
+
+  for(int i = 0; i < image.channels(); i++) {
+    Mat neighbor_intensities = (Mat_<float>(2, 2) << f11[i], f12[i], f21[i], f22[i]);
+    Mat result = x_weights * neighbor_intensities * y_weights;
+    interpolated_value[i] = result.at<float>(0, 0);
+  }
+
+  return interpolated_value;
+}
+
+Mat copyPerspectively(Mat to, Mat from, Mat homography, std::vector <Point2f> points) {
   int dstHeight = to.size().height;
   int dstWidth = to.size().width;
+  int srcHeight = from.size().height;
+  int srcWidth = from.size().width;
 
-  Mat world = Mat(dstHeight, dstWidth, to.type());
-  Mat mapX = Mat(dstHeight, dstWidth, CV_32FC1);
-  Mat mapY = Mat(dstHeight, dstWidth, CV_32FC1);
+  Mat dst_x = Mat(dstHeight, dstWidth, CV_32FC1);
+  Mat dst_y = Mat(dstHeight, dstWidth, CV_32FC1);
+  Mat transformed_src = Mat(dstHeight, dstWidth, to.type());
 
   for(int x = 0; x < dstWidth; x++) {
     for(int y = 0; y < dstHeight; y++) {
-      const float homogeneousPoint[3] = {(float)x, (float)y, 1};
-      Mat imagePoint = homography * Mat(3, 1, CV_32FC1, (void*)&homogeneousPoint);
-      float imageZ = imagePoint.at<float>(2, 0);
-      float imageX = imagePoint.at<float>(0, 0)/imageZ;
-      float imageY = imagePoint.at<float>(1, 0)/imageZ;
-      if(imageX > 0 && imageY > 0 && imageX < dstWidth && imageY < dstHeight) {
-        mapX.at<float>(y, x) = imageX;
-        mapY.at<float>(y, x) = imageY;
+      Point2f src_point = transform_point(homography, x, y);
+      // Effectively doesn't interpolate the borders and replaces them with 0s
+      if(src_point.x > 0 && src_point.y > 0 && src_point.x < srcWidth - 1 && src_point.y < srcHeight - 1) {
+        dst_x.at<float>(y, x) = src_point.x;
+        dst_y.at<float>(y, x) = src_point.y;
+        transformed_src.at<Vec3b>(y, x) = interpolate(from, src_point);
       } else {
         // Setting the x and y values in the maps to a value outside the image
         // causes them to be treated as "outliers" in the image and the default
         // BORDER_CONSTANT flag maps outliers to 0
-        mapX.at<float>(y, x) = dstWidth + 1;
-        mapY.at<float>(y, x) = dstHeight + 1;
+        dst_x.at<float>(y, x) = dstWidth + 1;
+        dst_y.at<float>(y, x) = dstHeight + 1;
+        transformed_src.at<Vec3b>(y, x) = Vec3b(0, 0, 0);
       }
     }
   }
-  remap(from, world, mapX, mapY, INTER_LINEAR);
-  world.copyTo(to, construct_roi_mask(dstHeight, dstWidth, points));
+
+  Mat final_image = to.clone();
+  transformed_src.copyTo(final_image, construct_roi_mask(dstHeight, dstWidth, points));
+  return final_image;
 }
 
 int main(int argc, char** argv )
@@ -63,7 +105,7 @@ int main(int argc, char** argv )
 
   Mat homography = computeHomography(dst_points, src_points);
 
-  copyPerspectively(dst_image, src_image, homography, dst_points);
-  write(argv[1], dst_image);
+  Mat modified_image = copyPerspectively(dst_image, src_image, homography, dst_points);
+  write(argv[1], modified_image);
   return 0;
 }
