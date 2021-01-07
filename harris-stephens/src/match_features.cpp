d1abb57cbec2d59c20e844a50052f57e8db1ba55
Ashima <ashimaathri@gmail.com>
2021-01-07 16:32:35 -0500

    Do ssd over a patch, fix non-maximal suppression

--- harris-stephens/src/match_features.cpp
+++ harris-stephens/src/match_features.cpp
@@ -124,12 +124,39 @@ void opencv_harris(Mat image) {
   display(result);
 }
 
-vector<KeyPoint> harris_stephens_corners(Mat image, float k, float threshold) {
-  Mat grayscale_img;
-  cvtColor(image, grayscale_img, COLOR_RGB2GRAY);
+Mat non_maximal_suppression(Mat values) {
+  Mat local_maximas = values.clone();
 
-  Mat Ix = convolve(grayscale_img, SOBEL_X, CV_32FC1);
-  Mat Iy = convolve(grayscale_img, SOBEL_Y, CV_32FC1);
+  int size = 20;
+
+  for(int r = 0; r < local_maximas.rows - size; r += size) {
+    for(int c = 0; c < local_maximas.cols - size; c += size) {
+      float max_value = -numeric_limits<float>::infinity();
+      int r_offset, c_offset;
+      for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+          float current = local_maximas.at<float>(r + i, c + j);
+          if(current > max_value) {
+            max_value = current;
+            r_offset = i;
+            c_offset = j;
+          }
+        }
+      }
+      Mat patch = local_maximas(Rect(c, r, size, size));
+      patch.setTo(Scalar::all(0));
+      patch.at<float>(r_offset, c_offset) = max_value;
+    }
+  }
+
+  return local_maximas;
+}
+
+vector<KeyPoint> harris_stephens_corners(Mat image, float k, float threshold) {
+  assert(image.channels() == 1);
+
+  Mat Ix = convolve(image, SOBEL_X, CV_32FC1);
+  Mat Iy = convolve(image, SOBEL_Y, CV_32FC1);
 
   Mat Ixx, Iyy, Ixy;
 
@@ -137,11 +164,13 @@ vector<KeyPoint> harris_stephens_corners(Mat image, float k, float threshold) {
   multiply(Ix, Iy, Ixy);
   multiply(Iy, Iy, Iyy);
 
-  boxFilter(Ixx, Ixx, Ixx.depth(), Size(2, 2), Point(-1, -1), true,
+  Size smoothing_size = Size(3, 3);
+
+  boxFilter(Ixx, Ixx, Ixx.depth(), smoothing_size, Point(-1, -1), true,
       BORDER_DEFAULT);
-  boxFilter(Ixy, Ixy, Ixy.depth(), Size(2, 2), Point(-1, -1), true,
+  boxFilter(Ixy, Ixy, Ixy.depth(), smoothing_size, Point(-1, -1), true,
       BORDER_DEFAULT);
-  boxFilter(Iyy, Iyy, Iyy.depth(), Size(2, 2), Point(-1, -1), true,
+  boxFilter(Iyy, Iyy, Iyy.depth(), smoothing_size, Point(-1, -1), true,
       BORDER_DEFAULT);
 
   Size size = Ixx.size();
@@ -160,6 +189,7 @@ vector<KeyPoint> harris_stephens_corners(Mat image, float k, float threshold) {
 
   Mat R_normalized;
   normalize(R, R_normalized, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
+  R_normalized = non_maximal_suppression(R_normalized);
 
   vector<KeyPoint> corners;
   for(int i = 0; i < R_normalized.rows; i++) {
@@ -180,29 +210,63 @@ Mat create_composite_image(Mat image1, Mat image2) {
   return composite;
 }
 
-vector<tuple<KeyPoint, KeyPoint>> get_matches_ssd(
+// TODO: handle case where points are at the borders of the image
+float ssd(Mat image1, Point2f point1, Mat image2, Point2f point2) {
+  assert(image1.channels() == 1 && image2.channels() == 1);
+
+  // Patch size = half_patch_size * 2 + 1
+  int half_patch_size = 10;
+  float result = 0;
+
+  for(int i = -half_patch_size; i <= half_patch_size; i++) {
+    for(int j = -half_patch_size; j <= half_patch_size; j++) {
+      float diff = (image1.at<char>(point1.x + i, point1.y + j) -
+          image1.at<char>(point2.x + i, point2.y + j));
+      result += diff * diff;
+    }
+  }
+
+  return result;
+}
+
+bool sort_by_score(
+    tuple <float, KeyPoint, KeyPoint> &a,
+    tuple <float, KeyPoint, KeyPoint> &b) {
+  return get<0>(a) < get<0>(b);
+}
+
+vector<tuple<KeyPoint, KeyPoint>> get_matches(
     Mat image1,
     vector<KeyPoint> corners1,
     Mat image2,
-    vector<KeyPoint> corners2) {
-  vector<tuple<KeyPoint, KeyPoint>> matches;
+    vector<KeyPoint> corners2,
+    float(*distance)(Mat, Point2f, Mat, Point2f)) {
+  vector<tuple<float, KeyPoint, KeyPoint>> matches;
 
   for(auto it1 = corners1.begin(); it1 < corners1.end(); it1++) {
     float min_distance = numeric_limits<float>::infinity();
-    tuple<KeyPoint, KeyPoint> best_pair;
+    tuple<float, KeyPoint, KeyPoint> best_pair;
+    auto best_position = corners2.begin();
     for(auto it2 = corners2.begin(); it2 < corners2.end(); it2++) {
-      Vec3b diff;
-      diff = image1.at<Vec3b>((*it1).pt.x, (*it1).pt.y) - image2.at<Vec3b>((*it2).pt.x, (*it2).pt.y);
-      float ssd = (diff[0] * diff[0]) + (diff[1] * diff[1]) + (diff[2] * diff[2]);
+      float ssd = distance(image1, (*it1).pt, image2, (*it2).pt);
       if(ssd < min_distance) {
         min_distance = ssd;
-        best_pair = make_tuple(*it1, *it2);
+        best_pair = make_tuple(min_distance, *it1, *it2);
+        best_position = it2;
       }
     }
+    corners2.erase(best_position);
     matches.push_back(best_pair);
   }
-  cout << matches.size() << endl;
-  return matches;
+  sort(matches.begin(), matches.end(), sort_by_score);
+
+  int i = 0;
+  int num_matches = 50;
+  vector<tuple<KeyPoint, KeyPoint>> top_matches;
+  for(auto it = matches.begin(); it < matches.end() && i < num_matches; it++, i++) {
+    top_matches.push_back(make_tuple(get<1>(*it), get<2>(*it)));
+  }
+  return top_matches;
 }
 
 int main(int argc, char** argv) {
@@ -213,11 +277,14 @@ int main(int argc, char** argv) {
 
   Mat image1 = imread(argv[1], 1);
   Mat image2 = imread(argv[2], 1);
+  Mat grayscale_img1, grayscale_img2;
+  cvtColor(image1, grayscale_img1, COLOR_RGB2GRAY);
+  cvtColor(image2, grayscale_img2, COLOR_RGB2GRAY);
 
   float k = atof(argv[3]);
   float threshold = atof(argv[4]);
-  vector<KeyPoint> corners1 = harris_stephens_corners(image1, k, threshold);
-  vector<KeyPoint> corners2 = harris_stephens_corners(image2, k, threshold);
+  vector<KeyPoint> corners1 = harris_stephens_corners(grayscale_img1, k, threshold);
+  vector<KeyPoint> corners2 = harris_stephens_corners(grayscale_img2, k, threshold);
 
   Mat result;
   drawKeypoints(image1, corners1, result, Scalar(50));
@@ -227,13 +294,13 @@ int main(int argc, char** argv) {
 
   // Draw line between matches
   Mat composite_image = create_composite_image(image1, image2);
-  vector<tuple<KeyPoint, KeyPoint>> matches = get_matches_ssd(image1, corners1, image2, corners2);
+  vector<tuple<KeyPoint, KeyPoint>> matches = get_matches(grayscale_img1, corners1, grayscale_img2, corners2, &ssd);
 
   Point2f offset = Point2f(image1.cols, 0);
   for(auto it = matches.begin(); it < matches.end(); it++) {
     KeyPoint kp1 = get<0>(*it);
     KeyPoint kp2 = get<1>(*it);
-    line(composite_image, kp1.pt, kp2.pt + offset, Scalar(30, 0, 30), 2, LINE_8);
+    line(composite_image, kp1.pt, kp2.pt + offset, Scalar(255), 2, LINE_8);
   }
 
   display(composite_image);
