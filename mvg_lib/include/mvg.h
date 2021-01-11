78f53de76faea3c8c999e835eb5c726647c37494
Ashima <ashimaathri@gmail.com>
2021-01-11 17:07:35 -0500

    Moving common functions to the library

--- mvg_lib/include/mvg.h
+++ mvg_lib/include/mvg.h
@@ -11,11 +11,14 @@ using namespace std;
 Mat computeScaling(Mat image, Mat homography);
 Mat changePerspective(Mat image, Mat homography);
 Mat computeHomography(std::vector <Point2f> from, std::vector <Point2f> to);
-void write(char* nameOfOriginal, Mat image, string suffix = string("-corrected"));
+void write(char* nameOfOriginal, Mat image, string suffix =
+    string("-corrected"));
 void display(Mat image);
-void receivePointCorrespondence(std::vector <Point2f> &points, const std::string &type);
+void receivePointCorrespondence(std::vector <Point2f> &points, const
+    std::string &type);
 Mat readHomogeneousCoordinates();
-Mat getNullSpace(void* linearSystem, int numRows, int numCols, int finalRows = 3);
+Mat getNullSpace(void* linearSystem, int numRows, int numCols,
+    int finalRows = 3);
 void printVector(vector <float> v);
 Mat normalize(Mat coordinates);
 Mat choleskyDecomposition(Mat matrix);
@@ -27,3 +30,12 @@ Mat solveConicSystem(vector<Mat> points);
 Mat getConic();
 Mat intersectLineAndConic(Mat conic, Mat P, Mat Q);
 Mat transformConic(Mat pointTransform, Mat conic);
+vector<KeyPoint> harris_stephens_corners(Mat image, float k, float threshold);
+Mat create_composite_image(Mat image1, Mat image2);
+float ssd(Mat image1, Point2f point1, Mat image2, Point2f point2);
+vector<tuple<KeyPoint, KeyPoint>> get_matches(
+    Mat image1,
+    vector<KeyPoint> corners1,
+    Mat image2,
+    vector<KeyPoint> corners2,
+    float(*distance)(Mat, Point2f, Mat, Point2f));
